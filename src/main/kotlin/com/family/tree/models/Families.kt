d5eb3ea2dd35a3b0eb705fcb90049996781ad178
Laura Coombs <lcoombs@kr.af.mil>
2020-09-21 17:24:47 -0600

    family model

--- src/main/kotlin/com/family/tree/models/Families.kt
+++ src/main/kotlin/com/family/tree/models/Families.kt
@@ -6,12 +6,12 @@ import java.util.*
 import javax.persistence.*
 
 @Entity
+@Table(name = "families")
 data class Families (
     @Id
     @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
     var familyId: UUID = UUID.fromString("8180eda9-bf97-4106-b170-f7c4a2d08f10"),
     var familyName: String = "Egbert",
     var familyDescription: String = "Blah",
-    var familyDateFrom: LocalDate = LocalDate.parse("2020-09-15"),
-    var headOfFamily: UUID = UUID.fromString()
+    var familyDateFrom: LocalDate = LocalDate.parse("2020-09-15")
 )
